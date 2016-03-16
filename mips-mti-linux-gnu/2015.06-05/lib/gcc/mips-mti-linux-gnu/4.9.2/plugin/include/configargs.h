051b1a8afddb98ef9f6ea9f6e554419131f3f2f8
TySag <2368962312@qq.com>
2016-03-16 20:37:07 +0800

    cross compiler

---
+++ mips-mti-linux-gnu/2015.06-05/lib/gcc/mips-mti-linux-gnu/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/user/mipsswt/git/gcc/configure --prefix=/user/mipsswt/build-2015.06-05/install-mips-mti-linux-gnu --disable-libssp --disable-libmudflap --disable-decimal-float --with-mips-plt --target=mips-mti-linux-gnu --enable-languages=c,c++ --with-sysroot=/user/mipsswt/build-2015.06-05/install-mips-mti-linux-gnu/sysroot --enable-__cxa_atexit --with-bugurl=http://imgtec.com/mips-sdk-support/ --with-pkgversion='Codescape GNU Tools 2015.06-05 for MIPS MTI Linux'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch_32", "mips32r2" }, { "arch_64", "mips64r2" }, { "llsc", "llsc" }, { "mips-plt", "plt" } };
